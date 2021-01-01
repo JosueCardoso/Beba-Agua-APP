1f4823239f22a0f170af49fe0511d984d9fdb8bf
Josue Cardoso <josuegcardoso@outlook.com>
2021-01-01 00:12:26 -0300

    Alterando nome da pasta de pages para screens

---
+++ src/screens/onboarding/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container, Text } from './styles';
+
+const home: React.FC = () => {
+    return (
+        <Container>
+            <Text>Onboarding</Text>
+        </Container>
+    );
+}
+
+export default home;
