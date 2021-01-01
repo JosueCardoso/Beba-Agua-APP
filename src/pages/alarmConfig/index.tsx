d933784368acc26b274d359a216d5738c60ebf72
Josue Cardoso <josuegcardoso@outlook.com>
2021-01-01 00:05:29 -0300

    Adicionando tela de configurações de alarme

---
+++ src/pages/alarmConfig/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container, Text } from './styles';
+
+const alarmConfig: React.FC = () => {
+    return (
+        <Container>
+            <Text>Alarm Config</Text>
+        </Container>
+    );
+}
+
+export default alarmConfig;
