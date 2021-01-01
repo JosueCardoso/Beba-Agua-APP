35cf09947a689da7f5329d20e896081f5b7e1910
Josue Cardoso <josuegcardoso@outlook.com>
2020-12-31 23:34:55 -0300

    first commit

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Container, Text } from './styles';
+
+const home: React.FC = () => {
+  return (
+  <Container>
+    <Text>Teste</Text>
+  </Container>);
+}
+
+export default home;
